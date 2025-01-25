73a2b8f0bcfd347221e33586a0c749a99b0f0788
aidil <aidil.ikbar@kpk.go.id>
2025-01-25 14:39:22 +0100

    fix route

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\AppointmentController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('health-records')->group(function () {
-    Route::get('health-records', [HealthRecordController::class, 'apiIndex']);
-    Route::post('health-records', [HealthRecordController::class, 'apiStore']);
-    Route::get('health-records/{id}', [HealthRecordController::class, 'apiShow']);
-    Route::put('health-records/{id}', [HealthRecordController::class, 'apiUpdate']);
-    Route::delete('health-records/{id}', [HealthRecordController::class, 'apiDestroy']);
+    Route::get('/', [HealthRecordController::class, 'apiIndex']);
+    Route::post('/', [HealthRecordController::class, 'apiStore']);
+    Route::get('/{id}', [HealthRecordController::class, 'apiShow']);
+    Route::put('/{id}', [HealthRecordController::class, 'apiUpdate']);
+    Route::delete('/{id}', [HealthRecordController::class, 'apiDestroy']);
 });
 
 Route::prefix('appointments')->group(function () {
